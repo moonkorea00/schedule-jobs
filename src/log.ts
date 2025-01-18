d1554cc6a303cb1b2404159f285590b297cecde9
Jeewon Moon <moonkorea00@gmail.com>
2025-01-18 15:39:46 +0900

    Update log.ts

--- src/log.ts
+++ src/log.ts
@@ -1,6 +1,7 @@
 function log() {
   fetch('https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query=맞춤법검사기').then(res => res.text()).then(res => {
     const match = /passportKey=([a-zA-Z0-9]+)/.exec(res)
+console.log('match : ', match)
     if (match) return match[1]}).catch(err => console.log('api err', err))
   fetch('https://jsonplaceholder.typicode.com/todos/1').then(res => res.json()).then(console.log).catch(err => console.log('err',err))
 }
